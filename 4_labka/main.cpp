d60342e32cd7cfbcf63ab4f551d9123261cc0f6e
Vladyslav Pasichnyk <114983141+WebVladPasichnyk@users.noreply.github.com>
2023-04-13 10:25:03 +0300

    Second_version

--- 4_labka/main.cpp
+++ 4_labka/main.cpp
@@ -1,23 +1,111 @@
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ofstream myFile("text.txt");
+
+class Myclass
+{
+public:
+   float y, xMin, xMax;
+
+   void func()
+   {
+      for (float x = -3; x <= 4; x += 0.1)
+      {
+         if (x > -4 && x < -3)
+         {
+            y = 3;
+         }
+         else if (x >= -3 && x < 0)
+         {
+            y = -x;
+         }
+         else if (x >= 0 && x < 2)
+         {
+            y = x;
+         }
+         else if (x >= 2 && x < 4)
+         {
+            y = 2;
+         }
+         cout << "  " << x << endl;
+         cout << "  " << y << endl;
+
+         myFile << x << "\t" << y << endl;
+
+         // myFile << "  " << x << endl;
+         // myFile << "  " << y << endl;
+      }
+   }
+};
 
 int main()
 {
-   float x, xMin, xMax, yMin, yMax;
-   xMax = 2;
-   xMin = -3;
-   yMax = 3;
-   yMin = 2;
-   x = xMin;
-   FILE *text = fopen();
-   while (x <= xMax)
-   {
-      fprintf(text, "\n%f", y);
-      fprintf(text, "   %f", x);
-      printf("Result: y = %f", y);
-      printf(" x = %f\n", x);
-      x = x + 0.5;
-   }
-   fclose(text);
+   Myclass obj;
+   obj.xMin = -4;
+   obj.xMax = 4;
+   obj.func();
+   myFile.close();
+
    return 0;
 }
+
+// #include <stdio.h>
+// #include <math.h>
+// #include <iostream>
+// #include <fstream>
+
+// using namespace std;
+
+// class Myclass
+// {
+// public:
+//    float y, xMin, xMax;
+
+//    void func()
+//    {
+//       ofstream myFile("text.txt"); // відкрийте файл для запису
+//       if (myFile.is_open())
+//       {
+//          for (float x = -4; x <= 4; x += 0.1)
+//          {
+//             if (x > -4 && x < -3)
+//             {
+//                y = 3;
+//             }
+//             else if (x >= -3 && x <= 0)
+//             {
+//                y = -x;
+//             }
+//             else if (x >= 0 && x < 2)
+//             {
+//                y = x;
+//             }
+//             else if (x >= 2 && x < 4)
+//             {
+//                y = 2;
+//             }
+//             cout << " " << x << " , " << y << endl;
+//             myFile << " " << x << " , " << y << endl; // записуйте результати у файл
+//          }
+//          myFile.close(); // закрийте файл після запису
+//       }
+//       else
+//       {
+//          cout << "Unable to open file." << endl;
+//       }
+//    }
+// };
+
+// int main()
+// {
+//    Myclass obj;
+//    obj.xMin = -4;
+//    obj.xMax = 4;
+//    obj.func();
+//    return 0;
+// }
